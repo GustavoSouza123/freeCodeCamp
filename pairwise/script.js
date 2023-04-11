17c12a83e62a1fbd4531c534f12acd9ab254cbc9
Gustavo Souza <gustavoelia7@gmail.com>
2023-04-11 09:42:38 -0300

    modificações no script.js pairwise (último commit no lab1)

--- pairwise/script.js
+++ pairwise/script.js
@@ -1,33 +1,49 @@
 function pairwise(arr, arg) {
     let ids = [];
+    let numIterations = 0;
     let aux = 1;
     while(aux > 0) {
         console.log('inicio while')
+        console.log('array: '+arr)
         let initialLength = arr.length;
-        for(i = 0; i < initialLength-1; i++) {
-            for(j = 0; j < initialLength; j++) {
-                if(i != j) {
-                    if(arr[i] + arr[j] == arg) {
-                        ids.push(i, j);
-                        arr.splice(j, 1);
-                        arr.splice(i, 1);
-                        console.log('push '+i+' e '+j);
-                        console.log('arr modificado: '+arr);
-                        console.log('arr ids: '+ids)
-                        break;
+        if(arr.length == 0) {
+            console.log('retornando');
+            aux--;
+            return 0;
+        } else {
+            for(i = 0; i < initialLength-1; i++) {
+                for(j = 0; j < initialLength; j++) {
+                    if(i != j) {
+                        if(arr[i] + arr[j] == arg) {
+                            ids.push(i, j);
+                            arr.splice(j, 1);
+                            arr.splice(i, 1);
+                            console.log('push '+i+' e '+j);
+                            console.log('arr modificado: '+arr);
+                            console.log('arr ids: '+ids)
+                            numIterations = 0;
+                            break;
+                        }
                     }
                 }
+                if(initialLength != arr.length) {
+                    console.log('initial length != arr.length');
+                    break;
+                }
+                if(i == initialLength-2) {
+                    console.log('i == initial length - 2')
+                    aux--;
+                }
             }
-            if(initialLength != arr.length) {
-                console.log('initial length != arr.length');
-                break;
-            }
-            if(i == initialLength-2) {
-                console.log('i == initial length - 2')
+            numIterations++;
+            if(numIterations == 3) {
+                console.log('retornando');
                 aux--;
+                let sum = ids.reduce(function(acc, cur) {
+                    return acc + cur;
+                });
+                return sum;
             }
         }
     }
-    console.log('retornando');
-    return ids;
 }
