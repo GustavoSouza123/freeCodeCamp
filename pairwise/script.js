0bcfefc34737dbf74f35bd84521a92d6d6ce2c27
Gustavo Souza <gustavoelia7@gmail.com>
2023-04-11 09:52:27 -0300

    exclusão dos console.log

--- pairwise/script.js
+++ pairwise/script.js
@@ -3,11 +3,8 @@ function pairwise(arr, arg) {
     let numIterations = 0;
     let aux = 1;
     while(aux > 0) {
-        console.log('inicio while')
-        console.log('array: '+arr)
         let initialLength = ids.length;
         if(arr.length == 0) {
-            console.log('retornando');
             aux--;
             return 0;
         } else {
@@ -18,25 +15,19 @@ function pairwise(arr, arg) {
                             ids.push(i, j);
                             arr[i] = 999;
                             arr[j] = 999;
-                            console.log('push '+i+' e '+j);
-                            console.log('arr modificado: '+arr);
-                            console.log('arr ids: '+ids)
                             numIterations = 0;
                             break;
                         }
                     }
                 }
                 if(initialLength != ids.length) {
-                    console.log('initial length != ids.length => modificação no arr ids');
                     break;
                 }
                 if(i == arr.length-2) {
-                    console.log('i == initial length - 2')
                 }
             }
             numIterations++;
             if(numIterations == 3) {
-                console.log('retornando');
                 aux--;
                 let sum = ids.reduce(function(acc, cur) {
                     return acc + cur;
