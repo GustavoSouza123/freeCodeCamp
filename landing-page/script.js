50405e0102ed4607166d08c3b2753c7ae1f0a0de
Gustavo Souza <gustavoelia7@gmail.com>
2023-07-06 12:55:50 -0300

    header responsiveness

--- landing-page/script.js
+++ landing-page/script.js
@@ -4,7 +4,18 @@ const cs = (e) => document.querySelectorAll(e);
 window.onscroll = () => {
     if(window.scrollY > 50) {
         c('#header').classList.add('header-scroll');
+        c('.menu-mobile').classList.add('header-scroll');
     } else {
         c('#header').classList.remove('header-scroll');
+        c('.menu-mobile').classList.remove('header-scroll');
     }
+}
+
+c('.menu-toggle').onclick = () => {
+    if(c('.menu-mobile').style.display == 'flex') {
+        c('.menu-mobile').style.display = 'none';
+    } else {
+        c('.menu-mobile').style.display = 'flex';
+    }
+    c('.menu-toggle').classList.toggle('active');
 }
