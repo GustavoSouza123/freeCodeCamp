74fb693a834843a0c610f986160e5043182272b7
Gustavo Souza <gustavoelia7@gmail.com>
2024-06-08 13:11:53 -0300

    fcc forum leaderboard: html, css and fetching latest topics from fcc forum

---
+++ javascript/fcc-forum-leaderboard/script.js
@@ -0,0 +1,74 @@
+const forumLatest = 'https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json';
+const forumTopicUrl = 'https://forum.freecodecamp.org/t/';
+const forumCategoryUrl = 'https://forum.freecodecamp.org/c/';
+const avatarUrl = 'https://sea1.discourse-cdn.com/freecodecamp';
+const postsContainer = document.getElementById('posts-container');
+
+const timeAgo = time => {
+    const currentTime = new Date();
+    const lastPost = new Date(time);
+
+    const timeDifference = currentTime - lastPost;
+    const msPerMinute = 1000 * 60;
+
+    const minutes = Math.floor(timeDifference / msPerMinute);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    if(minutes < 60) {
+        return `${minutes}m ago`;
+    }
+    
+    if(hours < 24) {
+        return `${hours}h ago`;
+    }
+
+    return `${days}d ago`;
+}
+
+const fetchData = async () => {
+    try {
+        const res = await fetch(forumLatest);
+        const data = await res.json();
+        showLatestPosts(data);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+fetchData();
+
+const showLatestPosts = data => {
+    const { topic_list, users } = data;
+    const { topics } = topic_list;
+    postsContainer.innerHTML = topics.map(item => {
+        const {
+            id,
+            title,
+            views,
+            posts_count,
+            slug,
+            posters,
+            category_id,
+            bumped_at
+        } = item;
+
+        return `
+        <tr>
+            <td>
+                <p class="post-title">${title}</p>
+            </td>
+            <td></td>
+            <td>
+                ${posts_count - 1}
+            </td>
+            <td>
+                ${views}
+            </td>
+            <td>
+                ${timeAgo(bumped_at)}
+            </td>
+        </tr>
+        `;
+    }).join('');
+}
